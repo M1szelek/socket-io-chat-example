22288d89606a6f1ef0699b12f987c49388f84b46
Michal Matlega <m1sz3lek@gmail.com>
2017-03-29 00:23:22 +0200

    Users list

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var Chat = function(){
+    this.users = [];
+
+    this.addUser = function(_user){
+        this.users.push(_user);
+    }
+}
+
+
+var Message = function(_user, _content){
+    this.user = _user;
+    this.content = _content;
+}
+
+var User = function(_name){
+    this.name = _name;
+}
+
+
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+var chat = new Chat();
+
 io.on('connection', function(socket){
   io.emit('user connected', 'user connected');
+
+  socket.on('new user', function(nickname){
+    chat.addUser(nickname);
+
+    io.emit('users list', chat.users);
+  });
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -21,3 +49,7 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+
+
+
